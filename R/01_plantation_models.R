0210f97346c708fb24f61515d31c0b4b4c64255f
Eric Buhle <eric.buhle@noaa.gov>
2021-12-14 11:57:21 -0800

    Improve posterior predictive plots

--- R/01_plantation_models.R
+++ R/01_plantation_models.R
@@ -28,13 +28,13 @@ epl_raw <- read.csv(here("data", "ExperimentalPlantation_long.csv"), header = TR
 # function to diff a vector while leapfrogging NAs (in that or another vector of equal length)
 diffNA <- function(x, y = NULL) {
   if(is.null(y)) y <- x else stopifnot(length(x) == length(y))
-  indx <- !is.na(y)
+  indx <- which(!is.na(y))
   dx <- x[indx] - lag(x[indx])
   replace(rep(NA, length(x)), indx, dx)
 }
 
 # add column for transect
-# calculate annual growth rate in total height and height above substrate
+# calculate annual growth rate (cm/yr) in total height and height above substrate
 epl <- epl_raw %>% rename(microhabitat = treat_simp) %>% 
   mutate(transect = sapply(strsplit(group, "_"), function(x) x[1]), .after = patch) %>% 
   arrange(seedling, year) %>% group_by(seedling) %>%
@@ -85,7 +85,7 @@ summary(lmer_gtht2)
 
 
 #---------------------------------------------------------------------------
-# DIAGNOSTIC PLOTS
+# DIAGNOSTIC POSTERIOR PREDICTIVE PLOTS
 #---------------------------------------------------------------------------
 
 # Simulate draws from posterior predictive distribution
@@ -93,39 +93,40 @@ mod <- lmer_gtht1
 yrep <- posterior_predict(mod)
 yrep <- yrep[sample(nrow(yrep)),]
 
-# Posterior predictive check: density overlay
-ppc_dens_overlay(mod$y, yrep[1:100,])
+# Posterior predictive check: density overlay grouped by patch
+ppc_dens_overlay_grouped(mod$y, yrep[1:100,], group = epl$patch[as.numeric(rownames(mod$glmod$fr))])
 
 # Posterior predictive check: histograms
 ppc_hist(mod$y, yrep[sample(nrow(yrep), 3),])
 
-# Posterior predictive check: mean
+# Posterior predictive check: mean grouped by transect
 ppc_stat_grouped(mod$y, yrep, group = mod$glmod$fr$transect, stat = mean)
 
-# Posterior predictive check: SD
+# Posterior predictive check: SD grouped by transect
 ppc_stat_grouped(mod$y, yrep, group = mod$glmod$fr$transect, stat = sd)
 
 # Normal QQ plot of transect-level random intercept point estimates, grouped by patch
 ranef(mod)$transect %>% rename(intercept = `(Intercept)`) %>% 
-  mutate(patch = factor(tapply(epl$patch, epl$transect, unique))) %>% 
-  ggplot(aes(sample = intercept)) + stat_qq(size = 2) + geom_qq_line() +
+  mutate(patch = epl$patch[match(row.names(.), epl$transect)]) %>% 
+  ggplot(aes(sample = intercept)) + stat_qq(pch = 1, size = 2) + geom_qq_line() +
   theme_bw() + theme(panel.grid = element_blank()) + facet_wrap(vars(patch), ncol = 2)
 
 # Normal QQ plot of seedling group-level random intercept point estimates, grouped by patch
 ranef(mod)$group %>% rename(intercept = `(Intercept)`) %>% 
   mutate(patch = epl$patch[match(row.names(.), epl$group)]) %>% 
-  ggplot(aes(sample = intercept)) + stat_qq(size = 2) + geom_qq_line() +
+  ggplot(aes(sample = intercept)) + stat_qq(pch = 1, size = 2) + geom_qq_line() +
   theme_bw() + theme(panel.grid = element_blank()) + facet_wrap(vars(patch), ncol = 2)
 
 # Normal QQ plot of seedling-level random intercept point estimates, grouped by patch
 ranef(mod)$seedling %>% rename(intercept = `(Intercept)`) %>% 
   mutate(patch = epl$patch[match(row.names(.), epl$seedling)]) %>% 
-  ggplot(aes(sample = intercept)) + stat_qq(size = 2) + geom_qq_line() +
+  ggplot(aes(sample = intercept)) + stat_qq(pch = 1, size = 2) + geom_qq_line() +
   theme_bw() + theme(panel.grid = element_blank()) + facet_wrap(vars(patch), ncol = 2)
 
 # Normal QQ plot of observation-level residuals point estimates, grouped by patch
 data.frame(mod$glmod$fr, resid = resid(mod)) %>% 
-  ggplot(aes(sample = resid)) + stat_qq(size = 2) + geom_qq_line() +
+  mutate(patch = epl$patch[as.numeric(rownames(.))]) %>% 
+  ggplot(aes(sample = resid)) + stat_qq(pch = 1, size = 2) + geom_qq_line() +
   theme_bw() + theme(panel.grid = element_blank()) + facet_wrap(vars(patch), ncol = 2)
 
 
